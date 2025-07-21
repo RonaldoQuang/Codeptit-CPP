9ebbc00a71c3f104c54834ddb101b5777333ac53
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-07-22 00:00:36 +0700

    Update CPP0143-SO FIBONACCI THU N.cpp

--- CPP0143-SO FIBONACCI THU N.cpp
+++ CPP0143-SO FIBONACCI THU N.cpp
@@ -21,8 +21,7 @@ int main(){
 		long long int a[100];
 		a[0]=1;
 		a[1]=1;
-		for(int i=2;i<92;i++)
-			a[i]=a[i-2]+a[i-1];
+		for(int i=2;i<92;i++) a[i]=a[i-2]+a[i-1];
 		cout << a[n-1] << endl;
 	}
 }
