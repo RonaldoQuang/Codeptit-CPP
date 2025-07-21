8580b7a65fbc892608ef04eef5ec6b97c300a9d1
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-07-22 00:18:37 +0700

    Update CPP0126-LIET KE SO NGUYEN TO-2.cpp

--- CPP0125-LIET KE SO NGUYEN TO-2.cpp
+++ CPP0125-LIET KE SO NGUYEN TO-2.cpp
@@ -10,14 +10,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int nt(int n){
-	for(int i=2;i<=sqrt(n);i++){
-		if(n%i==0) return 0;
-	}
-	return n>1;
+    for(int i=2;i<=sqrt(n);i++){
+	if(n%i==0) return 0;
+    }
+    return n>1;
 }
 int main(){
-	int t; cin >> t;
-	while(t--){
+    int t; cin >> t;
+    while(t--){
         int a, b; cin >> a >> b;
         for(int i=a;i<=b;i++) {
             if(nt(i)) cout << i << " ";
